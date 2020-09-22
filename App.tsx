9a55b8817aefeb5eb3de06bcaba52d3c36da80aa
Burhan Yılmaz <wburhany@gmail.com>
2020-09-22 16:05:37 +0300

    Space added.

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import {
   SafeAreaView,
   StatusBar,
 } from 'react-native';
-import { Avatar, Card } from '@educa/components';
+import { Avatar, Card, Space } from '@educa/components';
 
 const avatarUri = "https://avatars2.githubusercontent.com/u/37181742?s=460&u=304f0ea911bf02aa46f8ada9ad35d47cc1d3ca1b&v=4";
 
@@ -16,6 +16,7 @@ const App = () => {
           size={80}
           uri={avatarUri}
         />
+        <Space value={10}/>
         <Card title='Burhan Yilmaz'/>
       </SafeAreaView>
     </>
