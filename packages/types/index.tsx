51acd2b3e465787b3881e9b3aaaf393fb90ef887
Burhan Yılmaz <wburhany@gmail.com>
2020-09-23 12:10:42 +0300

    added types

---
+++ packages/types/index.tsx
@@ -0,0 +1,14 @@
+export interface User {
+  id: number;
+  name: string;
+}
+
+export interface Category {
+  id: number;
+  name: string;
+}
+
+export interface CourseClass {
+  id: number;
+  name: string;
+}
